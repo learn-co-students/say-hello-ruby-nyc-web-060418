4eb3ce1c557f37c65d95487e6ac314635510be51
Changmin Lim <chang.lim92@gmail.com>
2018-05-24 05:44:52 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,8 @@
 # Build your say_hello method here
 
-def say_hello()
-  puts "Hello Ruby Programmer!"
-end
+#def say_hello()
+#  puts "Hello Ruby Programmer!"
+#end
 
 def say_hello(name)
   puts "Hello #{name}!"
